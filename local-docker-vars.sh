c7e6462f14a5f2e38a88ed5e1d10487840eec19c
Skyler-Arnold <sky.Arnold@fit-pay.com>
2020-04-18 12:20:45 -0600

    add local docker env vars

---
+++ local-docker-vars.sh
@@ -0,0 +1,4 @@
+export LOCAL_DOCKER=/Users/arnoldskyler/Developer/microservices/local-docker-platform/
+export WEBAPP_HOME=/Users/arnoldskyler/Developer/
+export SERVICES_HOME=/Users/arnoldskyler/Developer/microservices/
+
