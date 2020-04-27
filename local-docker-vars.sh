01268b0aa8b8746e145012e6752eb2aacb1ddbc1
Skyler-Arnold <sky.Arnold@fit-pay.com>
2020-04-27 10:39:22 -0600

    fix local docker vars path

--- local-docker-vars.sh
+++ local-docker-vars.sh
@@ -1,4 +1,4 @@
-export LOCAL_DOCKER=/Users/arnoldskyler/Developer/microservices/local-docker-platform/
+export LOCAL_DOCKER=/Users/arnoldskyler/Developer/microservices/
 export WEBAPP_HOME=/Users/arnoldskyler/Developer/
 export SERVICES_HOME=/Users/arnoldskyler/Developer/microservices/
 
